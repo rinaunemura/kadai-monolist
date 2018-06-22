13c596c12694b1926b3e7878c8243d19cd0f5a8c
rina.unemura <rina.unemura@rakuten.com>
2018-06-22 04:14:23 +0000

    have_button

---
+++ resources/views/items/have_button.blade.php
@@ -0,0 +1,11 @@
+@if (Auth::user()->is_having($item->code))
+    {!! Form::open(['route' => 'item_user.dont_have', 'method' => 'delete']) !!}
+        {!! Form::hidden('itemCode', $item->code) !!}
+        {!! Form::submit('持ってない', ['class' => 'btn btn-info']) !!}
+    {!! Form::close() !!}
+@else
+    {!! Form::open(['route' => 'item_user.have']) !!}
+        {!! Form::hidden('itemCode', $item->code) !!}
+        {!! Form::submit('持ってる', ['class' => 'btn btn-warning']) !!}
+    {!! Form::close() !!}
+@endif
